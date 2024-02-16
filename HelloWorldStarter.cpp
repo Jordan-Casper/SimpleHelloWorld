5b60699d113433924948904fe10571132c6888b8
Jordan-Casper <14353592+Jordan-Casper@users.noreply.github.com>
2024-02-15 21:35:46 -0700

    first commit

---
+++ HelloWorldStarter.cpp
@@ -0,0 +1,27 @@
+// C++ program to display "Hello World"
+ 
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+ 
+// Main() function: where the execution of
+// program begins
+int main()
+{
+    // Prints hello world
+    cout << "Hello World \n";
+    cout << "The fish is smelly.\n";
+    int s = 1;
+    cout << "Same ";
+    cout << "Line";
+    cout << "\n";
+
+    cout << "This program is simply for testing my VSCode development environment for C++.\n";
+    
+
+    std::string message = std::to_string(s) + " this is the number one.";
+    std::cout << message << std::endl;
+    return 0;
+
+    
+}
